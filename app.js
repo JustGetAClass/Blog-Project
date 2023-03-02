1d614856842d5b3fce0e5847617ee8a6cfd1afb0
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-02 12:03:45 +0300

    created partials folder

--- app.js
+++ app.js
@@ -12,12 +12,17 @@ const contactContent =
 	"Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const port = 3000;
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.listen(3000, function () {
-	console.log("Server started on port 3000");
+app.get("/", (req, res) => {
+  res.render("home", {startingContent: homeStartingContent});
+});
+
+app.listen(port, function () {
+	console.log(`listening at port ${port}`);
 });
